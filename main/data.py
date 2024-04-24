a3d29a0714430a6e1ca71da894c05bfcfe17fbfc
Jakub Kiebala <kiebala.jakub@gmail.com>
2024-04-24 14:32:33 +0200

    exchanging rabbits and sheeps operations added

--- main/data.py
+++ main/data.py
@@ -22,3 +22,18 @@ class UserAnimals:
 
     def add_animal(self, animal):
         self.owned_animals.append(animal)
+
+    def exchange_rabbit_sheep(self):
+        for i in range(6):
+            self.owned_animals.remove('Królik')
+        self.add_animal('Owca')
+
+    def exchange_sheep_rabbit(self):
+        self.owned_animals.remove('Owca')
+        for i in range(6):
+            self.add_animal('Królik')
+
+    def exchange_sheep_pig(self):
+        for i in range(2):
+            self.owned_animals.remove('Owca')
+        self.add_animal('Świnia')
--- main/mechanics.py
+++ main/mechanics.py
@@ -106,7 +106,7 @@ def second_dice_operating_u1(drawn1, drawn2):
     drawn2_counted = FARM1.owned_animals.count(drawn2)
     if drawn2 != drawn1:
         if drawn2_counted >= 2:
-            FARM1.add_animal(drawn1)
+            FARM1.add_animal(drawn2)
             print(f'|#| Dodano {drawn2} w liczbie (1)')
         else:
             print(f'|#| Nie dodano {drawn2}')
@@ -138,11 +138,11 @@ def ask_for_exchange_u1():
 
 def choosing_operations_u1(do_ask):
     if do_ask == 1:
-        print('Operacje na królikach')
+        rabbit_operations()
     elif do_ask == 2:
-        print('Operacje na owcach')
+        sheep_operations()
     elif do_ask == 3:
-        print('Operacje na świniach')
+        pig_operations()
     elif do_ask == 4:
         print('Operacje na krowach')
     elif do_ask == 5:
@@ -151,3 +151,87 @@ def choosing_operations_u1(do_ask):
         print('Powrót do menu')
         make_choice_u1(type_choice_u1())
 
+
+def rabbit_operations():
+    counted_rabbits = FARM1.owned_animals.count('Królik')
+    if counted_rabbits < 6:
+        print('|#| Nie posiadasz wystarczającej ilości Królików \n'
+              '    aby wykonać jakąkolwiek wymianę')
+        choosing_operations_u1(ask_for_exchange_u1())
+    elif counted_rabbits >= 6:
+        print('|#| Możesz wymienić 6 królików na 1 Owcę')
+        print('|#| y - tak / n - nie')
+        while True:
+            question = str(input('|#| Czy chcesz dokonać wymiany? : '))
+            if question not in ('y', 'n'):
+                print('|#| Podano złą wartość, spróbuj jeszcze raz')
+            elif question == 'y':
+                FARM1.exchange_rabbit_sheep()
+                print('|#| Otrzymano 1 Owcę')
+                rabbit_operations()
+                return None
+            elif question == 'n':
+                choosing_operations_u1(ask_for_exchange_u1())
+                return None
+
+
+def sheep_operations():
+    counted_sheep = FARM1.owned_animals.count('Owca')
+    if counted_sheep < 1:
+        print('|#| Nie posiadasz wystarczającej ilości Owiec \n'
+              '    aby wykonać jakąkolwiek wymianę')
+        choosing_operations_u1(ask_for_exchange_u1())
+    elif 2 > counted_sheep >= 1:
+        print('|#| Możesz wymienić 1 Owcę na 6 Królików')
+        print('|#| y : tak / n : nie')
+        while True:
+            question = str(input('|#| Wybierz : '))
+            if question not in ('y', 'n'):
+                print('|#| Podano złą wartość, spróbuj jeszcze raz')
+            elif question == 'y':
+                FARM1.exchange_sheep_rabbit()
+                print('|#| Otrzymano 6 Królików')
+                sheep_operations()
+                return None
+            elif question == 'n':
+                choosing_operations_u1(ask_for_exchange_u1())
+                return None
+    elif counted_sheep >= 2:
+        print('|#| Możesz wymienić 1 Owcę na 6 Królików \n'
+              '    lub wymienić 2 Owce na 1 Świnię')
+        print('|#| 1 Owca => 6 Królików : 1 ')
+        print('|#| 3 Owce => 1 Świnia : 2 ')
+        print('|#| Wyjście : e ')
+        while True:
+            ask = str(input('|#| Wybierz : '))
+            if ask not in ('1', '2', 'e'):
+                print('|#| Podano złą wartość, spróbuj jeszcze raz')
+            elif ask == '1':
+                FARM1.exchange_sheep_rabbit()
+                print('|#| Otrzymano 6 Królików')
+                sheep_operations()
+                return None
+            elif ask == '2':
+                FARM1.exchange_sheep_pig()
+                print('|#| Otrzymano 1 Świnię')
+                sheep_operations()
+                return None
+            elif ask == 'e':
+                choosing_operations_u1(ask_for_exchange_u1())
+                return None
+
+
+def pig_operations():
+    pass
+
+
+
+
+
+
+
+for i in range(3):
+    FARM1.add_animal('Owca')
+
+rabbit_operations()
+print(FARM1.owned_animals)
